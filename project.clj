17370951de9b4ccb18383bf687770e3920a3c71f
Manoj Kumar A <tesni.manu@gmail.com>
2020-04-12 20:32:52 +0530

    Project configurations updated

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject documan "0.1.0-SNAPSHOT"
   :description "Tool to generate documents and diagrams."
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [dali "0.7.4"]]
   :main documan.core
   :aot [documan.core]
   :target-path "target/%s"
