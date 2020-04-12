61976a861c14ca40542f415c17d7f3ccf8cfad10
Manoj Kumar A <tesni.manu@gmail.com>
2020-04-12 12:50:40 +0530

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to documan
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/documan/core_test.clj
@@ -0,0 +1,7 @@
+(ns documan.core-test
+  (:require [clojure.test :refer :all]
+            [documan.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
