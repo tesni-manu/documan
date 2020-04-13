bce4765d720609b37b3e976d0cc8de2df5d6afaf
Manoj Kumar A <tesni.manu@gmail.com>
2020-04-14 00:12:59 +0530

    Large commit: refactored and WIP

--- src/documan/core.clj
+++ src/documan/core.clj
@@ -3,30 +3,55 @@
   (:require [clojure.string :as str]
             [documan.sequence-diagram :as seq]))
 
-(defn get-files-with-extension [path extension]
+(defn get-files-only-given-extension [path extension]
   (->> path
        clojure.java.io/file
        file-seq
        (filter #(.isFile %))
        (filter #(str/ends-with? (.getName %) extension))))
 
+(defn transpile-document
+  "Transpile the given source file and save it as edn in the specified path and filename."
+  [path filename document-data]
+  (let [lines (str/split-lines document-data)
+        first-line (first lines)
+        type (read-string first-line)
+        title (str/join " " (rest (str/split first-line #" ")))
+        other-lines (rest lines)]
+    (case type
+      ; TODO: add more types here
+      :sequence-diagram (seq/transpile-diagram
+                          (str path "/sequence-diagrams/")
+                          filename
+                          title
+                          other-lines))))
+
+(defn transpile-all-documents [project-path]
+  "Transpile edn files for all source code files in the project."
+  (println "Transpiling all in:" (str project-path "/src/"))
+  (doseq [file (get-files-only-given-extension (str project-path "/src/") ".documan")]
+    (transpile-document
+      (str project-path "/edn/")
+      (str/replace (.getName file) ".documan" "")
+      (slurp (.getAbsolutePath file)))))
+
 (defn generate-document
-  "Generate document and save it in the specified path and filename"
+  "Generate document and save it in the specified path and filename."
   [path filename document-data]
   (let [type (:type document-data)]
-    (case
+    (case type
       ; TODO: add more types here
       :sequence-diagram (seq/generate-diagram
                           (str path "/sequence-diagrams/")
                           filename document-data))))
 
 (defn generate-all-documents [project-path]
-  "Generates documents for all clj files in the project."
-  (println "Generating all in:" project-path)
-  (doseq [file (get-files-with-extension (str project-path "/clj/") ".clj")]
+  "Generates documents for all edn files in the project."
+  (println "Generating all in:" (str project-path "/edn/"))
+  (doseq [file (get-files-only-given-extension (str project-path "/edn/") ".edn")]
     (generate-document
       (str project-path "/dst/")
-      (str/replace (.getName file) ".clj" "")
+      (str/replace (.getName file) ".edn" "")
       (read-string (slurp (.getAbsolutePath file))))))
 
 (defn -main
@@ -34,13 +59,14 @@
   [& args]
   (let [project-path (first args)
         flags (vec (rest args))
-        flag-contains? (fn [flags val]
+        flag-contains? (fn [flags flag]
                          (or
                            (empty? flags)
-                           (contains? flags val)))]
+                           (not (zero? (count (filter #(= % flag) flags))))))]
     (if (empty? project-path)
       (println "Please specify the project path as first argument.")
       (do
-        ; TODO: add transpiling step here
+        (if (flag-contains? flags "-t")
+          (transpile-all-documents project-path))
         (if (flag-contains? flags "-g")
           (generate-all-documents project-path))))))
--- src/documan/sequence_diagram.clj
+++ src/documan/sequence_diagram.clj
@@ -1,16 +1,74 @@
 (ns documan.sequence-diagram
-  (:require [dali.io :as io]))
+  (:require [clojure.string :as str]
+            [dali.io :as io]
+            [dali.layout.stack]
+            [dali.layout.surround]))
 
-(defn create-sequence-diagram [diagram]
-  "Creates dali document for the sequence diagram with the given data"
-  [:dali/page
-   [:circle
-    {:stroke :indigo :stroke-width 4 :fill :darkorange}
-    [30 30] 20]])
+(defn create-edn
+  "Creates the diagram edn from the given source code."
+  [title source]
+  ; TODO
+  {:type :sequence-diagram :title title})
+
+(defn transpile-diagram
+  "Transpile the given source diagram into edn format."
+  [path filename title source]
+  (println "Transpiling diagram... " filename)
+  (let [edn (create-edn title source)]
+    (clojure.pprint/pprint edn (clojure.java.io/writer (str path filename ".edn")))))
+
+(defn create-sequence-diagram
+  "Creates dali document for the sequence diagram with the given data."
+  [diagram]
+  (let [width 1200
+        height 700
+        margin 40
+        padding 30
+        gap 5
+        body-left margin
+        body-top (+ margin gap)
+        body-width (- width (* 2 margin))
+        body-height (- height (* 2 margin))
+        title-color "#555"]
+    [:dali/page {:width  width
+                 :height height}
+     ;_____________________________________________________
+     ; title
+
+     [:text {:id          :title
+             :font-family "Sans Serif"
+             :stroke      {:paint title-color
+                           :width 0.3}
+             :fill        title-color
+             :font-size   16
+             :x           margin
+             :y           margin}
+      (:title diagram)]
+
+     ;_____________________________________________________
+     ; bounding rectangle
+
+     [:rect {:id     :bounding-rectangle
+             :stroke {:paint :black
+                      :width 0.2}
+             :fill   :white}
+      [body-left body-top]
+      [body-width body-height]]
+
+     ;_____________________________________________________
+     ; objects
+
+     ; TODO
+     ;_____________________________________________________
+     ; flow
+
+     ; TODO
+     ]))
 
 (defn generate-diagram
-  "Generates a sequence diagram with the given data in the specified path and filename"
+  "Generates a sequence diagram with the given data in the specified path and filename."
   [path filename diagram]
+  (println "Generating diagram... " filename)
   (io/render-svg
     (create-sequence-diagram diagram)
     (str path filename ".svg")))
