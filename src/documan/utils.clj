8cacbd699b3be59756755ed2dede3170b60e63c2
Manoj Kumar A <tesni.manu@gmail.com>
2020-04-29 22:59:11 +0530

    Refactored parser

--- src/documan/utils.clj
+++ src/documan/utils.clj
@@ -25,26 +25,19 @@
   [line]
   (let [chars (str/split line #"")
         not-blank-token? #(not (str/blank? %))
-        whitespace? (fn [char is-string-token]
-                      (and (not is-string-token)
-                           (or (= char ",") (str/blank? char))))
+        whitespace? (fn [char is-string-token] (and (not is-string-token) (or (= char ",") (str/blank? char))))
         collect-token (fn [coll char]
-                        (let [is-string-token (last coll)
-                              tokens (pop coll)]
+                        (let [is-string-token (last coll), tokens (pop coll)]
                           (if (whitespace? char is-string-token)
                             (conj tokens "" false)
                             (conj (pop tokens)
                                   (str (peek tokens) char)
-                                  (if (= "\"" char)
-                                    (not is-string-token)
-                                    is-string-token)))))
-        strip-double-quotes (fn [token]
-                              (if (= "\"" (subs token 0 1))
-                                (subs token 1 (dec (count token)))
-                                token))]
-    (mapv strip-double-quotes
-          (filterv not-blank-token?
-                   (reduce collect-token ["" false] chars)))))
+                                  (if (= "\"" char) (not is-string-token) is-string-token)))))
+        strip-double-quotes (fn [token] (if (= "\"" (subs token 0 1)) (subs token 1 (dec (count token))) token))]
+    (->> chars
+         (reduce collect-token ["" false])
+         (filterv not-blank-token?)
+         (mapv strip-double-quotes))))
 
 ;-----------------------------------------------------------------------------------------------------------------------
 
